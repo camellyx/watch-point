326f153ff99051748cda4e143abb54e72523ed46
camellyx <camellyx@80a8d319-df23-034c-51ae-6116052d3faa>
2011-05-02 19:08:14 +0000

    page table works

--- trunk/page_table_wp.h
+++ trunk/page_table_wp.h
@@ -8,6 +8,13 @@
 #ifndef PAGE_TABLE_WP_H_
 #define PAGE_TABLE_WP_H_
 
+#define	VIRTUAL_ADDRESS_LENGTH	32
+#define	PAGE_OFFSET_LENGTH		12
+#define	PAGE_NUMBER				(1<<12)
+#define	BIT_MAP_OFFSET_LENGTH	3
+#define	BIT_MAP_NUMBER_LENGTH	17
+#define	BIT_MAP_NUMBER			(1<<17)
+
 #include "auto_wp.h"
 
 template<class ADDRESS, class FLAGS>
@@ -16,10 +23,24 @@ public:
 	WatchPoint_PT();
 	~WatchPoint_PT();
 	
-	std::deque< watchpoint_t<ADDRESS, FLAGS> > wp;
-	typename std::deque< watchpoint_t<ADDRESS, FLAGS> >::iterator wp_iter;
-	typename std::deque< watchpoint_t<ADDRESS, FLAGS> >::iterator pre_iter;
-	typename std::deque< watchpoint_t<ADDRESS, FLAGS> >::iterator aft_iter;
+	//bool	general_fault	(ADDRESS start_addr, ADDRESS end_addr, FLAGS target_flags);
+	bool	watch_fault		(ADDRESS start_addr, ADDRESS end_addr);
+	//bool	read_fault		(ADDRESS start_addr, ADDRESS end_addr);
+	//bool	write_fault		(ADDRESS start_addr, ADDRESS end_addr);
+	
+	//typename std::deque<watchpoint_t<ADDRESS, FLAGS> >::iterator
+	//		search_address	(ADDRESS start_addr, std::deque<watchpoint_t<ADDRESS, FLAGS> > &wp);
+
+	void	watch_print();
+	
+	void	add_watchpoint	(ADDRESS start_addr, ADDRESS end_addr, FLAGS target_flags);
+	void	rm_watchpoint	(ADDRESS start_addr, ADDRESS end_addr, WatchPoint<ADDRESS, FLAGS> &wp);
+
+//private:
+	//called by rm_watchpoint to check the whole page in the WatchPoint system
+	bool	check_page_fault(ADDRESS page_number, WatchPoint<ADDRESS, FLAGS> &wp);
+
+	unsigned char bit_map[BIT_MAP_NUMBER];
 };
 
 #include "page_table_wp.cpp"
